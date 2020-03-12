76be0022dde454c3cf1ff42b3f96960a31aeeb19
Jay Fernandes <jay@plasticfernstudios.com>
2020-03-12 00:38:09 -0700

    Carousel

--- js/agency.js
+++ js/agency.js
@@ -3,7 +3,26 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
+ // Instantiate the Bootstrap carousel
+ $('.multi-item-carousel').carousel({
+   interval: false
+ });
 
+ // for every slide in carousel, copy the next slide's item in the slide.
+ // Do the same for the next, next item.
+ $('.multi-item-carousel .item').each(function(){
+   var next = $(this).next();
+   if (!next.length) {
+     next = $(this).siblings(':first');
+   }
+   next.children(':first-child').clone().appendTo($(this));
+
+   if (next.next().length>0) {
+     next.next().children(':first-child').clone().appendTo($(this));
+   } else {
+   	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+   }
+ });
 
  // retrieve the element
  var element = document.getElementById("contactus");
