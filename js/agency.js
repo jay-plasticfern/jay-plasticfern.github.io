4ebe6dec774b448e1f5a6550fc51239c09ea377e
Jay Fernandes <jay@plasticfernstudios.com>
2020-03-24 22:31:55 -0700

    nav bar fixes, carousel fixes

--- js/agency.js
+++ js/agency.js
@@ -3,25 +3,16 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
- // Instantiate the Bootstrap carousel
- $('.multi-item-carousel').carousel({
-   interval: false
- });
+ $(document).ready(function() {
+ 	$('#myCarousel').carousel({
+ 	interval: 0
+ 	})
 
- // for every slide in carousel, copy the next slide's item in the slide.
- // Do the same for the next, next item.
- $('.multi-item-carousel .item').each(function(){
-   var next = $(this).next();
-   if (!next.length) {
-     next = $(this).siblings(':first');
-   }
-   next.children(':first-child').clone().appendTo($(this));
+     $('#myCarousel').on('slid.bs.carousel', function() {
+     	//alert("slid");
+ 	});
 
-   if (next.next().length>0) {
-     next.next().children(':first-child').clone().appendTo($(this));
-   } else {
-   	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-   }
+
  });
 
  // retrieve the element
@@ -102,38 +93,7 @@
    element5.classList.add("jello");
  }, false);
 
- /* When the user clicks on the button,
- toggle between hiding and showing the dropdown content */
- function myFunction() {
-   document.getElementById("myDropdown").classList.toggle("show");
- }
- function myFunction2() {
-   document.getElementById("myDropdown2").classList.toggle("show");
- }
 
- // Close the dropdown menu if the user clicks outside of it
- window.onclick = function(event) {
-   if (!event.target.matches('.dropbtn')) {
-     var dropdowns = document.getElementsByClassName("dropdown-content");
-     var i;
-     for (i = 0; i < dropdowns.length; i++) {
-       var openDropdown = dropdowns[i];
-       if (openDropdown.classList.contains('show')) {
-         openDropdown.classList.remove('show');
-       }
-     }
-   }
-   if (!event.target.matches('.dropbtn2')) {
-     var dropdowns = document.getElementsByClassName("dropdown-content2");
-     var i;
-     for (i = 0; i < dropdowns.length; i++) {
-       var openDropdown = dropdowns[i];
-       if (openDropdown.classList.contains('show')) {
-         openDropdown.classList.remove('show');
-       }
-     }
-   }
- }
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
